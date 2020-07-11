7aa16750a73f994719bdb5bcca7a2db879320be3
Nirmaljeet Singh <500060993@stu.upes.ac.in>
2020-07-11 23:44:17 +0530

    Super Reduced problem

---
+++ Strings/Basic Operations.py
@@ -0,0 +1,17 @@
+i = input()
+ 
+s = []
+ 
+for c in i:
+    if not s:
+        s.append(c)
+    else:
+        if s[-1] == c:
+            s.pop()
+        else:
+            s.append(c)
+             
+if not s:
+    print ("Empty String")
+else:
+    print (s)
