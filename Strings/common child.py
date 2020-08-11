f544110147ebfe1cd2498c841e7e32cae9308572
Nirmaljeet Singh <500060993@stu.upes.ac.in>
2020-08-11 17:39:22 +0530

    Common child hackerrank

--- Strings/common child.py
+++ Strings/common child.py
@@ -1,19 +1,13 @@
-def commonchild(str1, str2):
-    a = list(str1)
-    b = list(str2)
-    count = 1
-    for i in range(0, len(a)):
-        for j in range(len(b)):
-            if a[i] == b[j]:
-                c = str1.index(a[i])
-                d = str2.index(b[j])
-                for r in range(c, len(a)):
-                    for s in range(d, len(b)):
-                        if a[r] == b[s]:
-                            count += 1
-            
-    return count
+def lcs(x,y,m,n):
+    if m == 0 or n == 0:
+        return 0
+    elif x[m-1] == y[n-1]:
+        return 1 + lcs(x,y, m-1, n-1)
+    else:
+        return max(lcs(x,y,m-1,n), (lcs(x,y,m,n-1)))
 
-str1 = "abcdef"
-str2 = "fbdamn"
-print(commonchild(str1, str2))
+x = input()
+y = input()
+m = len(x)
+n = len(y)
+print(lcs(x,y,m,n))
