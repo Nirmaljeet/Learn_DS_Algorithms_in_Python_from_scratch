acf3b10709d16bda730fcd4d8fd883d8c425f2b1
Nirmaljeet Singh <500060993@stu.upes.ac.in>
2020-09-03 15:29:05 +0530

    4 sum leetcode

--- arrays/4sum.py
+++ arrays/4sum.py
@@ -1,12 +1,10 @@
 def sum(arr,target):
-	for i in range(0, len(arr)-3):
+    	for i in range(0, len(arr)-3):
 		for j in range(i+1, len(arr)-2):
 			for k in range(j+1, len(arr)-1):
 				for l in range(k+1, len(arr)):
 					if arr[i]+arr[j]+arr[k]+arr[l] == target:
 						print(arr[i], arr[j], arr[k], arr[l])
-					else:
-    						return False
 					
 arr = [1, 0, -1, 0, -2, 2]
 target = 0
